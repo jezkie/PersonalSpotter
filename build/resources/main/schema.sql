3c58bccfba706e573bfebc5c7e7b6a6e8622ab5c
jezkie <jezkie00secondary@gmail.com>
2015-08-09 22:52:25 -0700

    Added SQLs
    HSQL DB script

---
+++ build/resources/main/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE CONTACTS(    
+  id  integer NOT NULL primary key,    
+  firstname    VARCHAR(30),   
+  lastname    VARCHAR(30),    
+  telephone   VARCHAR(15),    
+  email         VARCHAR(30));
+  
