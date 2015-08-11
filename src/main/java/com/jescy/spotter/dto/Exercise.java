5933883086832645683d52506792158ac74911b9
jezkie <jezkie00secondary@gmail.com>
2015-08-10 22:43:53 -0700

    Initial commit

---
+++ src/main/java/com/jescy/spotter/dto/Exercise.java
@@ -0,0 +1,63 @@
+package com.jescy.spotter.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "exercises")
+public class Exercise {
+	
+	private Integer id;
+	private String name;
+	private String description;
+	/**
+	 * @return the id
+	 */
+	@Id
+	@GenericGenerator(name="incr", strategy="increment")
+	@GeneratedValue(generator="incr")
+	@Column(name="id")
+	public Integer getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	@Column(name="name")
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the description
+	 */
+	@Column(name="description")
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
+}
